9f71191c645f2890cb5fbdb2f7ded3dda28ffac9
richardxnathan <richardnathand@gmail.com>
2024-02-11 16:26:28 -0300

    Aula 01

---
+++ FilmesApi/FilmesApi/Profiles/CinemaProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class CinemaProfile : Profile
+{
+
+    public CinemaProfile()
+    {
+        CreateMap<CreateCinemaDto, Cinema>();
+        CreateMap<Cinema, ReadCinemaDto>();
+        CreateMap<UpdateCinemaDto, Cinema>();
+    }
+
+}
